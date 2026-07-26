dfee7ec2972e9289326d90081112af1615e6d26e
suibari <404.not.found.address@gmail.com>
2026-07-26 12:42:06 +0900

    Vercel Analytics導入

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 export const ssr = false;
 export const prerender = false;
